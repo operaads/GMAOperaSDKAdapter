01aa87d6738ba72ccb19a431f2c2f060db82d589
luanchen <chenl@opera.com>
2026-03-11 21:29:21 +0800

    update demo

---
+++ Example/MediationExample/MediationExample-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  MediationExample-Bridging-Header.h
+//  MediationExample
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+// Import necessary headers for Swift access
+#import "AppDelegate.h"
+#import "ViewController.h"
+#import "StartViewController.h"
+#import "ExampleNativeAdView.h"
+#import "AdSourceConfig.h"
+#import "IDFAManager.h"
